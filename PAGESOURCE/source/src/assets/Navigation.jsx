4a9bfffaeea3a12e650efc6a325afb4cdc0478e4
Pranay Pal <pny1cl2125@gmail.com>
2023-04-02 01:16:53 +0530

    work

--- PAGESOURCE/source/src/assets/Navigation.jsx
+++ PAGESOURCE/source/src/assets/Navigation.jsx
@@ -1,2 +1,14 @@
 import * as React from 'react';
 import './Navigation.css';
+
+function Navbtns() {
+    return (
+        <div className="list" style="">
+        </div>
+        );
+}
+
+function Navigation() {
+    return ();
+}
+export default Navigation;
